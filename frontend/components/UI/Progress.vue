429047a6443afd30dd8b2e2b13b60a830767fc40
Terrero89 <sergio-terrero@hotmail.com>
2025-01-05 14:13:29 -0500

    added progress status started

---
+++ frontend/components/UI/Progress.vue
@@ -0,0 +1,214 @@
+<script setup lang="ts">
+const props = defineProps([
+  //PROPS HERE
+  "progressTitle",
+  "storyNumber",
+]);
+
+const titleTop = ref(
+    "Prevent conversation service API from opening a new conversacion for a new exposure on claim and phone numner."
+);
+const type1 = ref("FullStack");
+const type2 = ref("Frontend");
+// computer properties
+// const work = computed(() => {
+//   return workType.value ? 'fullstack' : 'frontend';
+// });
+</script>
+
+<template>
+  <div>
+    <div class="item-status">
+      <div class="status-title">
+        <h1>{{ progressTitle }}</h1>
+        <span class="story-number">{{ storyNumber }}</span>
+      </div>
+
+      <div class="story-list">
+        <div class="story">
+          <UTooltip text="More" :popper="{ arrow: true }" class="story-button">
+            <button class="">
+              <Icon name="i-heroicons-ellipsis-horizontal-circle" class=" icons"/>
+            </button>
+          </UTooltip>
+
+          <!--         + create a button to be able to move tasks to different status-->
+          <h1>{{ titleTop.substring(0, 30) }}</h1>
+          <div><span class="type">FullStack</span></div>
+
+          <div>
+            <Icon class="story-id doc icons " name="i-heroicons-document"/>
+            <span class="story-id">DMR-10427</span>
+            <UTooltip text="Story Points" :popper="{ arrow: true }">
+              <span class="story-points">3</span>
+            </UTooltip>
+            <UTooltip text="Repos" :popper="{ arrow: true }">
+              <button class="">
+                <Icon name="i-heroicons-document-check" class="icons"/>
+              </button>
+            </UTooltip>
+
+
+          </div>
+
+        </div>
+
+<!--        second story -->
+        <div class="story">
+          <UTooltip text="More" :popper="{ arrow: true }" class="story-button">
+            <button class="">
+              <Icon name="i-heroicons-ellipsis-horizontal-circle" class=" icons"/>
+            </button>
+          </UTooltip>
+
+          <!--         + create a button to be able to move tasks to different status-->
+          <h1>{{ titleTop.substring(0, 30) }}</h1>
+          <div><span class="type">Backend</span></div>
+
+          <div>
+            <Icon class="story-id doc icons " name="i-heroicons-document"/>
+            <span class="story-id">DMR-10427</span>
+            <UTooltip text="Story Points" :popper="{ arrow: true }">
+              <span class="story-points">3</span>
+            </UTooltip>
+            <UTooltip text="Repos" :popper="{ arrow: true }">
+              <button class="">
+                <Icon name="i-heroicons-document-check" class="icons"/>
+              </button>
+            </UTooltip>
+
+
+          </div>
+
+        </div>
+        <!--       third story -->
+        <div class="story">
+          <UTooltip text="More" :popper="{ arrow: true }" class="story-button">
+            <button class="">
+              <Icon name="i-heroicons-ellipsis-horizontal-circle" class=" icons"/>
+            </button>
+          </UTooltip>
+
+          <!--         + create a button to be able to move tasks to different status-->
+          <h1>{{ titleTop.substring(0, 30) }}</h1>
+          <div><span class="type">Frontend</span></div>
+
+          <div>
+            <Icon class="story-id doc icons " name="i-heroicons-document"/>
+            <span class="story-id">DMR-10427</span>
+            <UTooltip text="Story Points" :popper="{ arrow: true }">
+              <span class="story-points">3</span>
+            </UTooltip>
+            <UTooltip text="Repos" :popper="{ arrow: true }">
+              <button class="">
+                <Icon name="i-heroicons-document-check" class="icons"/>
+              </button>
+            </UTooltip>
+
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+.workType {
+  background-color: rgba(42, 185, 88, 0.75);
+}
+.item-status {
+  padding: 0.2rem 0;
+  margin: 0.5rem 0;
+  border-top: 1px solid rgba(236, 236, 236, 0.14);
+  border-bottom: 1px solid rgba(236, 236, 236, 0.14);
+  display: flex;
+  flex-direction: row;
+  background-color: rgb(15, 15, 15) !important;
+  border-radius: 5px;
+}
+
+
+.story-button {
+  border-radius: 5px;
+  float: right;
+}
+
+.story-list {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.status-title {
+  padding: 0.1rem;
+  min-width: 7rem;
+  margin: 0 0.5rem;
+  display: flex;
+
+  border-radius: 7px;
+}
+
+.status-title h1 {
+  margin: auto 0;
+  text-align: center;
+  align-content: center;
+}
+
+.story {
+  width: 13rem;
+  padding: 0.5rem;
+  margin-right: 0.5rem;
+  background-color: rgba(236, 236, 236, 0.14);
+  border: 1px solid rgba(236, 236, 236, 0.14);
+}
+
+.story h1 {
+  font-size: 0.75rem;
+  color: rgba(255, 255, 255, 0.77);
+}
+
+.story-top button {
+  margin-right: 0;
+}
+
+.type,
+.story-id,
+.story-points,
+.story-number {
+  color: rgba(208, 206, 206, 0.76);
+  font-size: 0.75rem;
+  font-weight: bold;
+  padding: 0.2rem 0.3rem;
+  border-radius: 0.2rem;
+}
+
+.story-number {
+  text-align: center;
+  margin: auto 0;
+}
+
+.type {
+  /*background-color: rgba(0, 183, 255, 0.75);*/
+  background-color: rgba(0, 183, 255, 0.75);
+}
+
+.icons {
+  color: rgba(208, 206, 206, 0.76);
+}
+
+.doc {
+  background-color: #0097ff;
+  padding: 0.2rem 0;
+
+}
+
+.story-points {
+
+  /*background-color: rgba(0, 183, 255, 0.75);*/
+}
+</style>
--- frontend/pages/stories/index.vue
+++ frontend/pages/stories/index.vue
@@ -1,8 +1,8 @@
 <script setup>
-import {useStoryStore} from "@/stores/stories";
+import { useStoryStore } from "@/stores/stories";
 
 const myStore = useStoryStore();
-const {fetchStories} = myStore;
+const { fetchStories } = myStore;
 const props = defineProps([
   //PROPS HERE
   "storyTitle",
@@ -24,7 +24,7 @@ const props = defineProps([
   "storyType",
   "learning",
   "planningNotes",
-  "updatedAt"
+  "updatedAt",
 ]);
 const {
   status,
@@ -36,36 +36,72 @@ const {
 </script>
 <template class="border-b border-gray-200">
   <div>
-    hello
-    <hr class="line"/>
+    STORY SECTION HERE
+    <!--    <hr class="line" />-->
 
-    <div class="">
-      <h1>STORY LIST</h1>
-      <StoryItem story-title="Story Title" story-description="Story Description" difficulty-level="Easy"
-                 story-points="2"
-                 work-type="feature" development-type="FE" status="To Do" :story-comments="['hello', 'cool']"
-                 subtasks="[{id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
-                 date="" reporter="Sergio Terrero" repo-names="['messaging-receive-api', 'C2T']"
-                 date-assigned="2024-12-28" date-completed="2024-12-30" sprint="PI2" story-type="C2T phone number setup"
-                 learning="[ {id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
-                 planning-notes="['note1', 'note2']" updated-at="2024-12-30"/>
-
+    <div class="progress">
+      <!--      component for progress in stories-->
+      <!--      to do, progress, done, demo ready, released-->
+      <!--      todos-->
+      <UIProgress progress-title="Backlog" story-number="2" />
+      <!--      todos-->
+      <UIProgress progress-title="To do" story-number="1" />
+      <!--      progress-->
+      <UIProgress progress-title="Progress" story-number="3" />
+      <!--      done-->
+      <UIProgress progress-title="Done" story-number="1" />
+      <UIProgress progress-title="Release" story-number="2" />
     </div>
-    <StoryItem story-title="Story Title" story-description="Story Description" difficulty-level="Easy" story-points="2"
-               work-type="feature" development-type="FE" status="To Do" :story-comments="['hello', 'cool']"
-               subtasks="[{id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
-               date="" reporter="Sergio Terrero" repo-names="['messaging-receive-api', 'C2T']"
-               date-assigned="2024-12-28" date-completed="2024-12-30" sprint="PI2" story-type="C2T phone number setup"
-               learning="[ {id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
-               planning-notes="['note1', 'note2']" updated-at="2024-12-30"/>
 
-
+    <h1>STORY LIST</h1>
+    <!-- <StoryItem
+       story-title="Story Title"
+       story-description="Story Description"
+       difficulty-level="Easy"
+       story-points="2"
+       work-type="feature"
+       development-type="FE"
+       status="To Do"
+       :story-comments="['hello', 'cool']"
+       subtasks="[{id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
+       date=""
+       reporter="Sergio Terrero"
+       repo-names="['messaging-receive-api', 'C2T']"
+       date-assigned="2024-12-28"
+       date-completed="2024-12-30"
+       sprint="PI2"
+       story-type="C2T phone number setup"
+       learning="[ {id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
+       planning-notes="['note1', 'note2']"
+       updated-at="2024-12-30"
+   /> -->
+    <!-- <StoryItem
+    story-title="Story Title"
+    story-description="Story Description"
+    difficulty-level="Easy"
+    story-points="2"
+    work-type="feature"
+    development-type="FE"
+    status="In Progress"
+    :story-comments="['hello', 'cool']"
+    subtasks="[{id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
+    date=""
+    reporter="Sergio Terrero"
+    repo-names="['messaging-receive-api', 'C2T']"
+    date-assigned="2024-12-28"
+    date-completed="2024-12-30"
+    sprint="PI2"
+    story-type="C2T phone number setup"
+    learning="[ {id: '1', title: 'hello', description: 'cool'}, {id: '2', title: 'hello', description: 'cool'}]"
+    planning-notes="['note1', 'note2']"
+    updated-at="2024-12-30"
+/> -->
   </div>
 </template>
 
 <style scoped>
-.cool {
-
+.progress {
+  background-color: #1f242a;
+  padding: 0.2rem;
 }
-
 </style>
