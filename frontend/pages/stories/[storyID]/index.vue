af90b43259333cdb6e157e39c45d556cb89b2d13
Terrero89 <sergio-terrero@hotmail.com>
2025-02-12 21:05:57 -0500

    fixedpage issue

---
+++ frontend/pages/stories/[storyID]/index.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <!-- Add your component template here -->
+    hl
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+
+
+
+
+// Lifecycle hook
+onMounted(() => {
+  
+});
+
+
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
+
