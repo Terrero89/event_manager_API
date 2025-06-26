6929d6c109e3d6ad48d0d4923080ba4faec63b54
Terrero89 <sergio-terrero@hotmail.com>
2025-06-26 18:48:43 -0400

    removed ignored files from cache

--- backend/config/configAPI.ts
+++
@@ -1,5 +0,0 @@
-export const API_CONFIG = {
-  LOCAL: "http://localhost:8080/api/v1",
-  PRODUCTION: "https://eventmanagerapi-dev.up.railway.app/api/v1",
-  DEVELOPMENT: "https://project-manager-app-f9829-default-rtdb.firebaseio.com",
-};
